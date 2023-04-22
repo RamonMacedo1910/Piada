5e275ae1d1f1cb9b78a1f718c3097a9e765b05a7
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-22 16:03:18 -0300

    Exercício cluído

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Piada de Futebol");
+
+Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("-Qual a diferença do Vasco para o Titanic?");
+            Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;    
+    Console.WriteLine("-Não sei, qual é?");
+            Console.ResetColor();
+    
+Console.ForegroundColor = ConsoleColor.Red;    
+    Console.WriteLine("-A difença é que o Titanic afundou no oceano,");
+    Console.WriteLine("e o Vasco no Rio kkkkkkkkkkkkkkk");
+            Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;    
+    Console.WriteLine("-Ah mano vai se f**** kkkkkkkkk");
+            Console.ResetColor();
